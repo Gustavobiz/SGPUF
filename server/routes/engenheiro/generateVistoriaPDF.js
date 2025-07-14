4cfaec5c6f3da5fb28d348b7eaea7fd4359881c5
Gustavobiz <gustavobernardes05@hotmail.com>
2025-07-14 01:21:10 -0300

    feat: template vistoria

---
+++ server/routes/engenheiro/generateVistoriaPDF.js
@@ -0,0 +1,19 @@
+const fs = require("fs-extra");
+const path = require("path");
+const handlebars = require("handlebars");
+const puppeteer = require("puppeteer");
+
+async function generateVistoriaPDF(data, outputPath) {
+  const templatePath = path.join(__dirname, "../templates/vistoria.hbs");
+  const html = await fs.readFile(templatePath, "utf-8");
+  const compileTemplate = handlebars.compile(html);
+  const finalHtml = compileTemplate(data);
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.setContent(finalHtml);
+  await page.pdf({ path: outputPath, format: "A4" });
+  await browser.close();
+}
+
+module.exports = generateVistoriaPDF;
