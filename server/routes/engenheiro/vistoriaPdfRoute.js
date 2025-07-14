658c7bc640ffa783c696996f6e3d16a5824e4e6e
Gustavobiz <gustavobernardes05@hotmail.com>
2025-07-14 01:39:16 -0300

    feat: com erros na busca vistoria

--- server/routes/engenheiro/vistoriaPdfRoute.js
+++ server/routes/engenheiro/vistoriaPdfRoute.js
@@ -4,7 +4,7 @@ const connection = require("../../config/db");
 const generatePDF = require("../../utils/generateVistoriaPDF");
 const path = require("path");
 
-router.get("/vistoria/pdf/:id", async (req, res) => {
+router.get("/:id/pdf", async (req, res) => {
   const idVistoria = req.params.id;
 
   try {
