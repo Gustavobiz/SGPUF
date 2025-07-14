bf09f43f315f4b9211e17fd1a228dd1c78a46ca2
Gustavobiz <gustavobernardes05@hotmail.com>
2025-07-14 09:40:48 -0300

    feat: vistoria pdf completo close #18

--- server/utils/generateVistoriaPDF.js
+++ server/utils/generateVistoriaPDF.js
@@ -11,6 +11,7 @@ async function generateVistoriaPDF(data, outputPath) {
 
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
+
   await page.setContent(finalHtml);
   await page.pdf({ path: outputPath, format: "A4" });
   await browser.close();
